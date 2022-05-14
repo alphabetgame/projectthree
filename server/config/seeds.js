d120b3e16f4521c5f0ba35ef19134b46a330fef8
Jason B <jasonsburke90@gmail.com>
2022-05-14 09:55:03 -0500

    added seeds

---
+++ server/config/seeds.js
@@ -0,0 +1,44 @@
+const db = require("./connection");
+const { User, Game } = require("../models");
+
+db.once("open", async () => {
+  await Game.deleteMany();
+
+  const game = await Game.insertMany([
+    { name: "Alphabet Game", solution: "ABCDEFGHIJKLMNOPQRSTUVWXYZ", level: 1 },
+    {
+      name: "Sentence Game",
+      solution: "THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG",
+      level: 2,
+    },
+  ]);
+
+  console.log("games seeded");
+
+  await User.deleteMany();
+
+  await User.insertMany([
+    {
+      firstName: "Chris",
+      lastName: "Angel",
+      email: "edgymagic@hotmail.com",
+      password: "davidblainesucks",
+    },
+    {
+      firstName: "Harry",
+      lastName: "Houdini",
+      email: "nowyouseeme@nowyoudont.com",
+      password: "imaghostnow",
+    },
+    {
+      firstName: "David",
+      lastName: "Blaine",
+      email: "davidblaine@blainemagic.com",
+      password: "magicismagic",
+    },
+  ]);
+
+  console.log("users seeded");
+
+  process.exit();
+});
