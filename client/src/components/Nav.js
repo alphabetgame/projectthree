722b369600d9b4ba621ea6efdb39311426f05b3a
Heather Stevens <heatherveva@gmail.com>
2022-05-21 14:00:10 -0400

    navigation bar working

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,79 +1,56 @@
 import React from "react";
 import Auth from "../utils/auth";
 import "../components/Nav.css";
+import { Link } from "react-router-dom";
 import Header from "./Header";
 import { Container, Navbar, Nav } from "react-bootstrap";
 
 function Navigation() {
   function showNavigation() {
-    if (Auth.loggedIn()) {
-      return (
-        // <nav className="nav">
-        //   <div className="container">
-        //     <div className="home-page-list">
-        //       <ul className="flex-row">
-        //         <li className="mx-1"></li>
-        //         <li className="mx-1">
-        //           {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-        //           <a href="/" onClick={() => Auth.logout()}>
-        //             Logout
-        //           </a>
-        //         </li>
-        //       </ul>
-        //     </div>
-        //   </div>
-        // </nav>
-        <Navbar bg="light" expand="lg">
-          <Container className="nav-bar">
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    const isLoggedIn = Auth.loggedIn();
+    return (
+      // <div className="container">
+      //   <ul className="flex-row">
+      //     <li className="mx-1">
+      //       <Link to="/">Home/Logo</Link>
+      //     </li>
+      //     <li className="mx-1">
+      //       <Link to="/signup">Signup</Link>
+      //     </li>
+      //     <li className="mx-1">
+      //       <Link to="/login">Login</Link>
+      //     </li>
+      //     <li className="mx-1">
+      //       <Link to="/game">Game</Link>
+      //     </li>
+      //   </ul>
+      // </div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand>
+            <Link to="/">Letter Scramble</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/" onClick={() => Auth.logout()}></Nav.Link>
+              <Link to="/signup">Sign Up</Link>
+
+              <Link to="/game">Game</Link>
+              {isLoggedIn ? (
+                <Link to="/" onClick={() => Auth.logout()}>
+                  Logout
+                </Link>
+              ) : (
+                <Link to="/login">Login</Link>
+              )}
             </Nav>
-            ]{" "}
-          </Container>
-        </Navbar>
-      );
-    } else {
-      return (
-        // <div className="container">
-        //   <ul className="flex-row">
-        //     <li className="mx-1">
-        //       <Link to="/">Home/Logo</Link>
-        //     </li>
-        //     <li className="mx-1">
-        //       <Link to="/signup">Signup</Link>
-        //     </li>
-        //     <li className="mx-1">
-        //       <Link to="/login">Login</Link>
-        //     </li>
-        //     <li className="mx-1">
-        //       <Link to="/game">Game</Link>
-        //     </li>
-        //   </ul>
-        // </div>
-        <Navbar bg="light" expand="lg">
-          <Container>
-            <Navbar.Brand>
-              <a href="#home">Letter Scramble</a>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Nav className="me-auto">
-              <Nav.Link eventKey="#home">Home</Nav.Link>
-              <Nav.Link eventKey="#signup">Sign Up</Nav.Link>
-              <Nav.Link eventKey="#login">Login</Nav.Link>
-              <Nav.Link eventKey="#game">Game</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-      );
-    }
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
   }
 
-  return (
-    <Navbar>
-      <Header>{showNavigation()}</Header>
-    </Navbar>
-  );
+  return <div>{showNavigation()}</div>;
 }
 
 export default Navigation;
