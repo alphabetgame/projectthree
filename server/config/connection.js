1e213c6ca6c9649f0abde6ca618e57ca4cf40dcd
Rachel <yarkony.rachel@gmail.com>
2022-05-24 18:14:51 -0500

    updated connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/alphabetgame",
+  "mongodb+srv://atlas:admin@cluster0.pxpmj.mongodb.net/alphabetDB?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
