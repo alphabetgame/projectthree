4c1e5f92daaebe16f45d8f1dbe54ebffce022707
Heather Stevens <heatherveva@gmail.com>
2022-05-24 15:50:11 -0500

    wording on home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,13 +7,19 @@ const Home = () => {
   return (
     <div className="container">
       <p className="about">
-        Welcome to Letter Scramble! Where you can play games to teach early
-        english spelling. Our alphabet game and our spelling game that goes
-        through spelling words up to a fourth grade level.
+        Welcome to Letter Scramble! These engaging word games are designed to
+        help kids practice their English spelling from home. Once you choose a
+        level, you'll see a word pop up on the screen. Then, the word will
+        disappear and reappear scrambled. Your job is to click the cards in the
+        correct order to spell the original word. Good luck and happy spelling!
       </p>
       <div className="about">
         <p>Try one of our games!</p>
-        <button><Link to="/game" className="game-l">Games</Link></button>
+        <button>
+          <Link to="/game" className="game-l">
+            Games
+          </Link>
+        </button>
       </div>
     </div>
   );
