ebfbc0bd19086f350fcb3bd611ef068f0c06adfd
Brian Holbrook <brian.holbrook56@gmail.com>
2022-05-24 15:10:19 -0500

    homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,16 @@
 import React from "react";
 //signup, login, description/directions of the game
+import { Link } from "react-router-dom";
+import './../components/Game.css'
 
 const Home = () => {
   return <div className="container">
-    <p>Here at Letter Scramble we use games to teach early english spelling. our alphabet game and our spelling game that goes through spelling words up to a fourth grade level</p>
+    <p className="instructs">Here at Letter Scramble we use games to teach early english spelling. our alphabet game and our spelling game that goes through spelling words up to a fourth grade level</p>
+    <div className="instructs">
+      <p>Try one of our games</p>
+      <button><Link to="/game" className="btn-l">Games</Link></button>
+
+    </div>
   </div>;
 };
 
