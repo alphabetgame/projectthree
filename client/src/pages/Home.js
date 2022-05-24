031e2d297985bdfc05ea086e52b63f0b01083bea
Rachel <yarkony.rachel@gmail.com>
2022-05-24 15:12:53 -0500

    styled about sec

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
+import "../components/Home.css";
 //signup, login, description/directions of the game
 
 const Home = () => {
-  return <div className="container">
-    <p>Here at Letter Scramble we use games to teach early english spelling. our alphabet game and our spelling game that goes through spelling words up to a fourth grade level</p>
-  </div>;
+  return (
+    <div className="container">
+      <p className="about">
+        Welcome to Letter Scramble! Where you can play games to teach early
+        english spelling. Our alphabet game and our spelling game that goes
+        through spelling words up to a fourth grade level.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
